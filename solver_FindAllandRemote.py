e529c0bc15317629eae02f1900fc8d71ae3e9360
Boyan Zhou <steven.b.zhou@foxmail.com>
2019-04-29 11:41:02 -0700

    clean some format

--- solver_FindAllandRemote.py
+++ solver_FindAllandRemote.py
@@ -24,7 +24,72 @@ def solve(client):
     client.start()
 
     # print(shortestPathfromHome(client)[1][::-1])
-    findAllBots(client)
+    vote = votes(client)
+    # print('The estimated accuracy of students is '+str(estimatedAccuracy(client, vote)))
+    vote = np.array(vote)
+    printVote(vote)
+    ac = estimatedAccuracy(client, vote)
+    print("Accuracy is: " + str(ac))
+    seq = vote.argsort()
+    seq = seq[::-1]
+    seq = seq + 1
+    count = 0
+
+    Q = []  # vertices in Q must be checked anyway
+    for i in range(client.l):
+        Q.append(seq[0])
+        seq = np.delete(seq, [0])
+    while (count < client.l - 1):
+        minW = 100000
+        for i in range(1, len(Q)):
+            if client.G.edges[Q[0], Q[i]]['weight'] < minW:
+                minW = client.G.edges[Q[0], Q[i]]['weight']
+                to = i
+        knownBots = client.bot_count[Q[0]]
+        frum = int(Q[0])
+        to = int(Q[to])
+        if client.G.edges[Q[0], client.h]['weight'] < minW:
+            to = client.h
+        # print("known bots: " + str(knownBots))
+        count = count + client.remote(frum, to) - knownBots
+        # print("Count is: " + str(count))
+        del Q[0]
+        if len(Q) < client.l - count:
+            nextAcc = calCorrectProb(client, vote, ac, seq[0])
+            if nextAcc < 0.6:
+                break
+            Q.append(seq[0])
+            seq = np.delete(seq, [0])
+
+    knownBots = client.bot_count[Q[0]]
+    count = count + client.remote(int(Q[0]), client.h) - knownBots
+    del Q[0]
+    print("Now directly remote home:")
+    print("Count is: " + str(count))
+
+    costinSeq = []
+    for v in Q:
+        seq = np.append(v, seq)
+    seq = seq.tolist()
+    seq.remove(client.h)
+    seq = np.array(seq)
+    for v in seq:
+        punishment = 1
+        if calCorrectProb(client, vote, ac, v) < 0.2:
+            punishment = 1.5
+        if v != client.h:
+            costinSeq.append(
+                (1 - calCorrectProb(client, vote, ac, v)) * client.G.edges[v, client.h]['weight'] * punishment)
+
+    costinSeq = np.array(costinSeq)
+    newSeq = costinSeq.argsort()
+
+    for i in newSeq:
+        # print("count: " + str(count))
+        if count == client.l:
+            break
+        knownBots = client.bot_count[int(seq[i])]
+        count = count + client.remote(int(seq[i]), client.h) - knownBots
     # remoteKnownBotHome(client)
 
     score = client.end()
