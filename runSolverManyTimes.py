81ab54dfddecbb5ae88284261059c57d8190ac23
韩笑尘 <hanx2021@berkeley.edu>
2019-04-29 13:26:06 -0700

    simplified the merging algorithm in candidate merging

--- runSolverManyTimes.py
+++ runSolverManyTimes.py
@@ -1,15 +1,19 @@
 from solver_FindAllandRemote import *
 from ModifiedClient import *
 
-N = 100
-c = Client(False)
+N = 50
+client = Client(False)
 score = 0
+client.end()
 
-for i in range(N):
-    print("Starting run #", i+1)
-    s = solve(c)
-    score += s
+for i in range(1, N + 1):
+    print("Starting run #", i)
+    cur = solve(client)
+    score += cur
+    print("Average score: ", score / i)
+    print('----------run finished----------')
+    print('')
 
 score /= N
 
-print('Have run ' + str(N) + ' tests, the average score is: ' + str(score))
+print('Finished running ' + str(N) + ' tests, final average score is: ' + str(score))
---
+++ solver_CandidateMerging.py
@@ -0,0 +1,99 @@
+import networkx as nx
+
+
+# python local_server.py
+# python client.py --solver solver_CandidateMerging
+# python runSolverManyTimes.py
+# average score: 91.79336560472193
+
+# invariants:
+#   the candidates list always contains client.l - counter candidates
+#   the candidates list is always sorted in increasing distance from home in same segment
+#   the candidates list never touches vertices with less than 50% probability
+
+
+def solve(client):
+    client.start()
+    vote_array = votes(client)
+
+    # sort the vertices based on student votes
+    vertices = list(range(client.v))
+    vertices.sort(key=lambda v: vote_array[v])
+    vertices.reverse()
+
+    # calculate the length of shortest path from each vertex to home
+    shortest_path_to_home = nx.single_source_shortest_path(client.G, client.h)
+    shortest_path_to_home_length = {}
+
+    for vertex in sorted(shortest_path_to_home):
+        distance = 0
+        path = shortest_path_to_home[vertex]
+        for e in range(len(path) - 1):
+            distance += client.G.edges[path[e], path[e + 1]]['weight']
+        shortest_path_to_home_length[vertex] = distance
+
+    # maintain a candidates list of length bots_left
+    bots_moved_to_home = 0
+    vertex_index = 0
+    candidates = []
+    while bots_moved_to_home < client.l:
+        # at the start of loop always refill candidates
+        for _ in range(client.l - bots_moved_to_home - len(candidates)):
+            candidates.append(vertices[vertex_index] + 1)
+            vertex_index += 1
+
+        # potential target vertex is the one closest to home
+        target = min(candidates, key=lambda c: shortest_path_to_home_length[c])
+        bots_moved_to_target = 0
+
+        # calculate distance to target from each candidate
+        shortest_path_to_target = nx.single_source_shortest_path(client.G, target)
+        shortest_path_to_target_length = {}
+        for candidate in candidates:
+            distance = 0
+            path = shortest_path_to_target[candidate]
+            for e in range(len(path) - 1):
+                distance += client.G.edges[path[e], path[e + 1]]['weight']
+            shortest_path_to_target_length[candidate] = distance
+
+        # remote each candidate to target or home depending on distance
+        for candidate in candidates:
+            if candidate == target:
+                continue
+            if shortest_path_to_target_length[candidate] < shortest_path_to_home_length[candidate]:
+                bots_moved_to_target += remote_path(client, shortest_path_to_target, candidate)
+                candidates.remove(candidate)
+            else:
+                bots_moved_to_home += remote_path(client, shortest_path_to_home, candidate)
+                candidates.remove(candidate)
+
+        # finally remote from target to home, potentially carrying many bots
+        bots_moved_to_home += remote_path(client, shortest_path_to_home, target)
+        candidates.remove(target)
+
+    score = client.end()
+    return score
+
+
+def votes(client):
+    vote_array = []
+    for v in range(client.v):
+        if v + 1 == client.h:
+            vote_array.append(0)
+        else:
+            results = client.scout(v + 1, list(range(1, client.k + 1)))
+            sum = 0
+            for s in range(client.k):
+                if results[s + 1]:
+                    sum += 1
+            vote_array.append(sum)
+    return vote_array
+
+
+def remote_path(client, shortest_paths, vertex):
+    path = shortest_paths[vertex]
+    path = path[::-1]
+    action = 0
+    for e in range(len(path) - 1):
+        action = client.remote(path[e], path[e + 1])
+    return action
